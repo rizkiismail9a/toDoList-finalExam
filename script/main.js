19fa1f61198d2eae150a1657732f262ff88ddca0
rizkiismail <laksmimentari@gmail.com>
2023-08-16 09:47:18 +0700

    added edit function

--- script/main.js
+++ script/main.js
@@ -56,6 +56,7 @@ const showLists = (data) => {
     <p>${item.time}</p>
     <h6>${item.date}</h6>
     <a style="color: white" onclick="deleteTask(${index})">Hapus</a>
+    <button style="background-color: transparent; border: none; color: white; font-weight: bold;" onclick="editTask(event, ${index})">Edit</button>
   </div>`;
   });
   listContainer.innerHTML = content;
@@ -122,11 +123,13 @@ const createTask = (e) => {
     return alert("Masukkan data terlebih dahulu");
   }
   const newTask = {
+    id: todoList.length + 1,
     date: inputDate,
     time: inputTime,
     todo: inputTodo,
     color,
   };
+  console.log(newTask);
   todoList.push(newTask);
   clearForm();
   showLists(todoList);
@@ -136,4 +139,63 @@ const searchTask = () => {
   const keyword = document.getElementById("search").value;
   const result = todoList.filter((item) => item.todo.toLowerCase().includes(keyword.toLowerCase()));
   showLists(result);
+};
+
+const editTask = (e, index) => {
+  document.getElementById("inputDate").value = document.querySelectorAll("h6")[index].textContent;
+  document.getElementById("inputTime").value = document.querySelectorAll("p")[index].textContent;
+  document.getElementById("inputTodo").value = document.querySelectorAll("h5")[index].textContent;
+  document.querySelector(".btn-primary").style.display = "none";
+  const tombolUpdate = document.createElement("button");
+  tombolUpdate.innerHTML = "Edit Memo";
+  tombolUpdate.classList = "btn btn-primary";
+  tombolUpdate.setAttribute("onclick", `updateMemo(event, ${index})`);
+  tombolUpdate.setAttribute("id", "editBuku");
+  document.querySelectorAll("div.col-12")[1].appendChild(tombolUpdate);
+};
+
+const updateMemo = (e, index) => {
+  e.preventDefault();
+  const inputDate = document.getElementById("inputDate").value;
+  const inputTime = document.getElementById("inputTime").value;
+  const inputTodo = document.getElementById("inputTodo").value;
+  const colorOption1 = document.getElementById("option1").checked;
+  const colorOption2 = document.getElementById("option2").checked;
+  const colorOption3 = document.getElementById("option3").checked;
+  const colorOption4 = document.getElementById("option4").checked;
+  const colorOption5 = document.getElementById("option5").checked;
+  const colorOption6 = document.getElementById("option6").checked;
+  const colorOption7 = document.getElementById("option7").checked;
+
+  let color = "";
+  if (colorOption1) {
+    color = "#607EAA";
+  } else if (colorOption2) {
+    color = "#AC7088";
+  } else if (colorOption3) {
+    color = "#EED180";
+  } else if (colorOption4) {
+    color = "#90C8AC";
+  } else if (colorOption5) {
+    color = "#90C8AC";
+  } else if (colorOption6) {
+    color = "#D8CCA3";
+  } else if (colorOption7) {
+    color = "#82A284";
+  }
+  if (!inputDate || !inputTime || !inputTodo) {
+    return alert("Masukkan data terlebih dahulu");
+  }
+  const newTask = {
+    id: todoList.length + 1,
+    date: inputDate,
+    time: inputTime,
+    todo: inputTodo,
+    color,
+  };
+  todoList.splice(index, 1, newTask);
+  document.querySelector(".btn-primary").style.display = "block";
+  document.getElementById("editBuku").remove();
+  clearForm();
+  showLists(todoList);
 };
